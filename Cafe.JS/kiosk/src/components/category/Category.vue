cc219041c622ccf6154e07aa5be5e6207503050c
phygloria <phygloria@gmail.com>
2024-06-05 15:59:26 +0900

    menu

--- Cafe.JS/kiosk/src/components/category/Category.vue
+++ Cafe.JS/kiosk/src/components/category/Category.vue
@@ -1,17 +1,33 @@
 <script setup>
+import { ref } from 'vue';
+
+const buttons = ref([
+  { label: '추천메뉴!', color: 'brown' },
+  { label: '커피', color: 'brown' },
+  { label: '디카페인', color: 'brown' },
+  { label: '스무디', color: 'brown' },
+  { label: '티&에이드', color: 'brown' },
+  { label: '디저트', color: 'brown' }
+]);
+
+const changeColor = (index) => {
+  // 모든 버튼의 색상을 갈색으로 초기화
+  buttons.value.forEach(button => {
+    button.color = 'brown';
+  });
+  // 클릭된 버튼의 색상을 노란색으로 변경
+  buttons.value[index].color = 'yellow';
+};
 
 </script>
 
 <template>
-    <div id="categoryMenu">
-        <div @click="" class="menuBtn">
-            <h4>추천메뉴!</h4>
-        </div>
-
-        <div @click="" class="menuBtn">
-            <h4>추천메뉴!</h4>
-        </div>
+     <div class="categoryMenu">
+    <div v-for="(button, index) in buttons" :key="index" @click="changeColor(index)" :class="['menuBtn', { yellow: button.color === 'yellow' }]">
+      <h4>{{ button.label }}</h4>
     </div>
+  </div>
+
 </template>
 
 
@@ -26,12 +42,12 @@
 
     .categoryMenu{
         display: flex;
-
+        padding-top: 10px;
+        justify-content: center; 
     }
-
-
-
+    
     .menuBtn{
+        
         display: flex;
         font-family: 'BMJUA';
         font-size: 1.15em;
@@ -41,12 +57,22 @@
         width: 100px;
         height: 50px;
         border-radius: 10px;
+        margin: 5px;        
         
         /* justify-content: center; 좌우 가운대 */
         justify-content: center; 
         /* align-items: center; 상하 가운대 */
         align-items: center;
+        /* float: left; 왼쪽부터 시작해서 가로로 나열 */
+        float: left;            
 
     }
+
+    .yellow {
+        background-color: #FFB834;
+        color: #783E19;
+    }
+
+
 
 </style>
